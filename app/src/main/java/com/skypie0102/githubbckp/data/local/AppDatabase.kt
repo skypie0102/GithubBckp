c4091fb9ed2a738135fa68679acc76851fbfb3c4
skypie0102 <skypirate1202@gmail.com>
2026-09-17 06:37:11 +0800

    db: bump schema for re-verification

--- app/src/main/java/com/skypie0102/githubbckp/data/local/AppDatabase.kt
+++ app/src/main/java/com/skypie0102/githubbckp/data/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 
 @Database(
     entities = [RepositoryEntity::class, BackupEntity::class],
-    version = 8,
+    version = 9,
     exportSchema = false,
 )
 @TypeConverters(Converters::class)
