1021cd83c8de3af415af30b2364c62b5057a91b6
skypie0102 <skypirate1202@gmail.com>
2026-09-16 14:34:07 +0800

    feat: register repository availability migration

--- app/src/main/java/com/skypie0102/githubbckp/data/local/DatabaseModule.kt
+++ app/src/main/java/com/skypie0102/githubbckp/data/local/DatabaseModule.kt
@@ -20,6 +20,7 @@ object DatabaseModule {
                 DatabaseMigrations.MIGRATION_1_2,
                 DatabaseMigrations.MIGRATION_2_3,
                 DatabaseMigrations.MIGRATION_3_4,
+                DatabaseMigrations.MIGRATION_4_5,
             )
             .build()
 
