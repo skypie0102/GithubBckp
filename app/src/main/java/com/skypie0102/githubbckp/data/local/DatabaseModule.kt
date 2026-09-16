bf650616f4a2bf9aea7d15e0b331f73f21e7a593
skypie0102 <skypirate1202@gmail.com>
2026-09-17 06:37:28 +0800

    db: register re-verification migration

--- app/src/main/java/com/skypie0102/githubbckp/data/local/DatabaseModule.kt
+++ app/src/main/java/com/skypie0102/githubbckp/data/local/DatabaseModule.kt
@@ -24,6 +24,7 @@ object DatabaseModule {
                 DatabaseMigrations.MIGRATION_5_6,
                 DatabaseMigrations.MIGRATION_6_7,
                 DatabaseMigrations.MIGRATION_7_8,
+                DatabaseMigrations.MIGRATION_8_9,
             )
             .build()
 
