a450a704c3f7036656a906d2b2321c0e40cf39dd
skypie0102 <skypirate1202@gmail.com>
2026-09-18 14:30:52 +0800

    release: prepare 0.3.0 refactor version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.skypie0102.githubbckp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.2"
+        versionCode = 4
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
