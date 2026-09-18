7c21b318bf50b61bdf318923d0dce68066dcbdd4
skypie0102 <skypirate1202@gmail.com>
2026-09-18 09:44:36 +0800

    Narrow GitHub gateway to repository discovery

--- app/src/main/java/com/skypie0102/githubbckp/github/GithubGateway.kt
+++ app/src/main/java/com/skypie0102/githubbckp/github/GithubGateway.kt
@@ -4,6 +4,4 @@ import com.skypie0102.githubbckp.backup.RepositoryRef
 
 interface GithubGateway {
     suspend fun listRepositories(): List<RepositoryRef>
-
-    suspend fun repositoryHasWiki(repository: RepositoryRef): Boolean
 }
