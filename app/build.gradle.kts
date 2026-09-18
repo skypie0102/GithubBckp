28fe08b4f72e98a3744c0ea3738f0171ff0b86ea
skypie0102 <skypirate1202@gmail.com>
2026-09-18 19:43:50 +0800

    release: bump browsable archive to 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.skypie0102.githubbckp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.2"
+        versionCode = 7
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
