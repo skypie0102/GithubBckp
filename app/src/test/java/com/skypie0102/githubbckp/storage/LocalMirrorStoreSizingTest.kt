0511e93024f224c8d34b92a3eaa4ae2c1cbf8df5
skypie0102 <skypirate1202@gmail.com>
2026-09-18 13:49:48 +0800

    test: cover mirror workspace sizing

---
+++ app/src/test/java/com/skypie0102/githubbckp/storage/LocalMirrorStoreSizingTest.kt
@@ -0,0 +1,41 @@
+package com.skypie0102.githubbckp.storage
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class LocalMirrorStoreSizingTest {
+    @Test
+    fun updateWorkspaceIncludesArchiveCopiesExpansionAndHeadroom() {
+        val mib = 1024L * 1024L
+        assertEquals(
+            2L * 100L * mib + 400L * mib + 100L * mib,
+            requiredUpdateWorkspaceBytes(
+                compressedBytes = 100L * mib,
+                expandedBytes = 400L * mib,
+            ),
+        )
+    }
+
+    @Test
+    fun updateWorkspaceUsesMinimumHeadroomForSmallMirrors() {
+        val mib = 1024L * 1024L
+        assertEquals(
+            2L * 10L * mib + 20L * mib + 64L * mib,
+            requiredUpdateWorkspaceBytes(
+                compressedBytes = 10L * mib,
+                expandedBytes = 20L * mib,
+            ),
+        )
+    }
+
+    @Test
+    fun updateWorkspaceSaturatesInsteadOfOverflowing() {
+        assertEquals(
+            Long.MAX_VALUE,
+            requiredUpdateWorkspaceBytes(
+                compressedBytes = Long.MAX_VALUE,
+                expandedBytes = Long.MAX_VALUE,
+            ),
+        )
+    }
+}
