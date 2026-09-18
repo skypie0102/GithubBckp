27f53867b9215ae336e7dd5384adb0b6a1aa51e7
skypie0102 <skypirate1202@gmail.com>
2026-09-18 13:51:18 +0800

    refactor: map repository access readiness

--- app/src/main/java/com/skypie0102/githubbckp/data/local/Mappers.kt
+++ app/src/main/java/com/skypie0102/githubbckp/data/local/Mappers.kt
@@ -2,11 +2,15 @@ package com.skypie0102.githubbckp.data.local
 
 import com.skypie0102.githubbckp.github.GithubRepository
 
-fun GithubRepository.toEntity(selectedForBackup: Boolean = true): RepositoryEntity = RepositoryEntity(
+fun GithubRepository.toEntity(
+    selectedForBackup: Boolean = true,
+    isAvailable: Boolean = true,
+): RepositoryEntity = RepositoryEntity(
     githubId = id,
     owner = owner,
     name = name,
     defaultBranch = defaultBranch,
     isPrivate = isPrivate,
     selectedForBackup = selectedForBackup,
+    isAvailable = isAvailable,
 )
