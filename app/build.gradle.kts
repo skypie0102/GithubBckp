b434e1ce4592291e388c89611ce70fdbd927a4a2
skypie0102 <skypirate1202@gmail.com>
2026-09-18 21:09:51 +0800

    release: bump latest release backup to 0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.skypie0102.githubbckp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.3.3"
+        versionCode = 8
+        versionName = "0.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
