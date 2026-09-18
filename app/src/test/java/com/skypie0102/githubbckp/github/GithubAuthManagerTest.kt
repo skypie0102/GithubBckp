0d79edcbd4db7b32d732e2cd7aa4b200ee7b4e46
skypie0102 <skypirate1202@gmail.com>
2026-09-18 14:19:04 +0800

    test: reject revoked and forbidden github tokens

--- app/src/test/java/com/skypie0102/githubbckp/github/GithubAuthManagerTest.kt
+++ app/src/test/java/com/skypie0102/githubbckp/github/GithubAuthManagerTest.kt
@@ -1,9 +1,19 @@
 package com.skypie0102.githubbckp.github
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GithubAuthManagerTest {
+    @Test
+    fun revokedOrForbiddenTokenResponsesAreRejected() {
+        assertTrue(githubTokenStatusAccepted(200))
+        assertTrue(githubTokenStatusAccepted(204))
+        assertFalse(githubTokenStatusAccepted(401))
+        assertFalse(githubTokenStatusAccepted(403))
+    }
+
     @Test
     fun normalizesPersonalAccessTokenInput() {
         assertEquals("github_pat_example", normalizePersonalAccessToken("  github_pat_example\n"))
