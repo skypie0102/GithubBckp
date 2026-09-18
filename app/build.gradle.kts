5190e5005657cc15835c5315b322d1abb5a3bdaa
skypie0102 <skypirate1202@gmail.com>
2026-09-18 08:55:24 +0800

    Bump backup cancellation hotfix to v0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.skypie0102.githubbckp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.1"
+        versionCode = 3
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
