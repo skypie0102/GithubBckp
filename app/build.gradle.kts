d68eadfd651fa80dc21c8987474377806d0946cd
skypie0102 <skypirate1202@gmail.com>
2026-09-19 20:29:22 +0800

    release: bump sequential queue UX to 0.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.skypie0102.githubbckp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.3.4"
+        versionCode = 9
+        versionName = "0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
